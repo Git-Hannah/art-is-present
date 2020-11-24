f2a22d626817a9eb2e290edea49d2566913e5c2f
Hortencia Jorquera <hortencia.pj@gmail.com>
2020-11-24 22:24:44 +0100

    products and artist connected

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,16 @@
-document.addEventListener('DOMContentLoaded', () => {
+// document.addEventListener('DOMContentLoaded', () => {
 
-  console.log('IronGenerator JS imported successfully!');
+//   console.log('IronGenerator JS imported successfully!');
 
-}, false);
+// }, false);
+
+
+const Product=require('../models/Product');
+
+function search_product() { 
+	let input = document.getElementById('searchbar').value.toLowerCase(); 
+  //Product.find( )//product whith names or descriptions that include the input
+  //.then(productList=>{
+      //res.render('categoryView',{productList})
+  //})
+}
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -3,15 +3,34 @@ const router=express.Router();
 const Product=require('../models/Product');
 const mongoose=require('mongoose');
 
+
 const {uploadCloud,cloudinary}=require('../configs/cloudinary.config');
 
 router.get('/edit/:id/product',(req,res,next)=>{
   Product.findById(req.params.id)
     .then(selectedProduct=>{
-      res.render('products/edit',{selectedProduct});
+      console.log(selectedProduct);
+      let selectedAvailability='';
+      let availabilityOptions='';
+      const availabilityArr=['available','currently unavailable'];
+      availabilityArr.forEach(el=>{
+        selectedAvailability= el===selectedProduct.availability ? 'selected':'';
+        availabilityOptions+=`<option value="${el}" ${selectedAvailability}>${el}</option>`;
+      })
+    
+
+      let categoryOptions='';
+      let selectedCategory='';
+      const categoriesArr=['Painting','Drawing','Photography','Performance','Music'];
+      categoriesArr.forEach(el=>{
+        selectedCategory= el===selectedProduct.category ? 'selected':'';
+        categoryOptions+=`<option value="${el}" ${selectedCategory}>${el}</option>`;
+      })
+
+      res.render('products/edit',{selectedProduct,availabilityOptions,categoryOptions});
     })
     .catch(err=>next(err))
-});
+}); 
 
 router.post('/edit/:id/product',uploadCloud.array('images'),(req,res,next)=>{
   const {name,price,availability,category,description}=req.body;
@@ -21,14 +40,12 @@ router.post('/edit/:id/product',uploadCloud.array('images'),(req,res,next)=>{
   editedProduct={name,price,availability,category,description}
   else
   editedProduct={name,price,availability,category,description,images}
-  //console.log(editedProduct)
   
 
   Product.findByIdAndUpdate(req.params.id,editedProduct)
   .then(foundProduct=>{
-    //console.log(foundProduct)
-      //res.render('',{message='product updated'})
-      console.log('product updated')
+    const price= new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(foundProduct.price);
+    res.render('products/show',{foundProduct,price})
     })
     .catch(err=>{
       if(err instanceof mongoose.Error.ValidationError){
@@ -41,16 +58,19 @@ router.post('/edit/:id/product',uploadCloud.array('images'),(req,res,next)=>{
 });
 
 router.get('/show/:id/product',(req,res,next)=>{
-  Product.findById(req.params.id)
+  Product.findById(req.params.id).populate('owner')
     .then(foundProduct=>{
-      res.render('products/show',{foundProduct})
-      console.log(foundProduct.images)
+      const price= new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(foundProduct.price);
+      res.render('products/show',{foundProduct,price})
+
     })
     .catch(err=>next(err))
 });
 
 router.get('/add/product',(req,res,next)=>{
+  console.log(req.session.passport.user)
   res.render('products/add');
+  
 });
 
 router.post('/add/product',uploadCloud.array('images'), (req,res,next)=>{
@@ -58,11 +78,12 @@ router.post('/add/product',uploadCloud.array('images'), (req,res,next)=>{
 
   const {name,price,availability,category,description}=req.body;
   const images=req.files.map(file=>file.path);
-  //const owner=req.session.... //get and set the owner's id
+  const owner=req.session.passport.user //get and set the owner's id
+  
 
-  Product.create({name,price,availability,category,description,images})
+  Product.create({name,price,availability,category,description,images,owner})
     .then(()=>{
-      //res.render('',{message:'Product Added!'}) //which view should we render here?
+      //res.render('products/show',{foundProduct={name,price,availability,description,category,owner}})
       console.log('product added');
       return;
     })
@@ -76,5 +97,13 @@ router.post('/add/product',uploadCloud.array('images'), (req,res,next)=>{
       }
     })
 });
+
+router.post('/delete/:id/product',(req,res,next)=>{
+  Product.findByIdAndDelete(req.params.id)
+   .then(()=>{
+     //res.render() //redirect to artist profile
+     console.log('product deleted');
+   })
+})
 
 module.exports =router;
