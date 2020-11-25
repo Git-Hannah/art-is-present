9dbe691b823e0aac0ae90cdf81210b2597a40c8e
Hannah <Viking8@gmx.de>
2020-11-25 20:40:53 +0100

    fix category details view

--- routes/index.js
+++ routes/index.js
@@ -10,30 +10,29 @@ router.get('/', (req, res, next) => {
 
 router.get('/categories/:categoryName', (req,res,next) => {
   const categoryName = req.params.categoryName;
-  Product.find({category: categoryName})
+  Product.find({category: categoryName}).populate("owner")
   .then (categoryProductList => {
     res.render('categories', {categoryProductList, categoryName, user: req.session.passport.user});
 
   }).catch (err => console.log(err))
 }) 
 
-router.post('/categories/:categoryName', (req, res, next) => {
-  const categoryName = req.params.categoryName;
-  const lower = +req.body.price.split("-")[0];
+// router.post('/categories/:categoryName', (req, res, next) => {
+//   const categoryName = req.params.categoryName;
+//   const lower = +req.body.price.split("-")[0];
 
-  if (req.body.price.split("-").length > 1)
-  const higher = +req.body.price.split("-")[1];
+//   if (req.body.price.split("-").length > 1)
+//   // const higher = +req.body.price.split("-")[1];
 
- // console.log(+req.body.price.split("-")[0]);
-  Product.find({category: categoryName, $and: [ { qty: { $lt: 100 } }, { qty: { $gt: 30 } } ]  })
-  .then (categoryProductList => {
-    res.render('categories', {categoryProductList, categoryName, user: req.session.passport.user});
+//  // console.log(+req.body.price.split("-")[0]);
+//   // Product.find({category: categoryName, $and: [ { qty: { $lt: 100 } }, { qty: { $gt: 30 } } ]  })
+//   // .then (categoryProductList => {
+//   //   res.render('categories', {categoryProductList, categoryName, user: req.session.passport.user});
 
-  }).catch (err => console.log(err))
-})
+//   // }).catch (err => console.log(err))
+// })
 
 
 module.exports = router;
 
 
-// query kveri 
