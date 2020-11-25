a1e5e7528d29821632f3cef8bc5106d9d0f8417c
Hannah <Viking8@gmx.de>
2020-11-25 18:04:40 +0100

    add filter of products by price

--- routes/index.js
+++ routes/index.js
@@ -12,10 +12,26 @@ router.get('/categories/:categoryName', (req,res,next) => {
   const categoryName = req.params.categoryName;
   Product.find({category: categoryName})
   .then (categoryProductList => {
-    res.render('categories', {categoryProductList})
+    res.render('categories', {categoryProductList, categoryName, user: req.session.passport.user});
 
   }).catch (err => console.log(err))
 }) 
+
+router.post('/categories/:categoryName', (req, res, next) => {
+  const categoryName = req.params.categoryName;
+  const lower = +req.body.price.split("-")[0];
+
+  if (req.body.price.split("-").length > 1)
+  const higher = +req.body.price.split("-")[1];
+
+ // console.log(+req.body.price.split("-")[0]);
+  Product.find({category: categoryName, $and: [ { qty: { $lt: 100 } }, { qty: { $gt: 30 } } ]  })
+  .then (categoryProductList => {
+    res.render('categories', {categoryProductList, categoryName, user: req.session.passport.user});
+
+  }).catch (err => console.log(err))
+})
+
 
 module.exports = router;
 
