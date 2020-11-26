b0417c2af272da3711a792023bdd5af35678a30c
ararita <munjizavesna@gmail.com>
2020-11-26 18:23:54 +0100

    Finish header styling

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Product = require("../models/Product");
-const Artist= require("../models/Artist");
+const Artist = require("../models/Artist");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
@@ -20,90 +20,97 @@ router.get("/categories/:categoryName", (req, res, next) => {
     .catch((err) => console.log(err));
 });
 
-router.post('/categories/:categoryName', (req, res, next) => {
+router.post("/categories/:categoryName", (req, res, next) => {
   const categoryName = req.params.categoryName;
   const user = req.session.passport ? req.session.passport.user : undefined;
 
-  if(req.body.price){
+  if (req.body.price) {
     //console.log(req.body)
-    if (req.body.price[0].split("-")[1]== ''){
+    if (req.body.price[0].split("-")[1] == "") {
       const lower = req.body.price[0].split("-")[0];
-      Product.find({category: categoryName, price: { $gte: lower }})
-      .then (categoryProductList => {
-        res.render('categories', {categoryProductList, categoryName, user});
-      })
-      .catch (err => console.log(err))
-      
-    }else{
+      Product.find({ category: categoryName, price: { $gte: lower } })
+        .then((categoryProductList) => {
+          res.render("categories", { categoryProductList, categoryName, user });
+        })
+        .catch((err) => console.log(err));
+    } else {
       const lower = req.body.price[0].split("-")[0];
       const higher = req.body.price[0].split("-")[1];
-      Product.find({category: categoryName, $and: [ { price: { $lte: higher } }, { price: { $gt: lower } } ]  })
-      .then (categoryProductList => {
-        //console.log(categoryProductList,higher,lower)
-        res.render('categories', {categoryProductList, categoryName, user});
+      Product.find({
+        category: categoryName,
+        $and: [{ price: { $lte: higher } }, { price: { $gt: lower } }],
       })
-      .catch (err => console.log(err))
+        .then((categoryProductList) => {
+          //console.log(categoryProductList,higher,lower)
+          res.render("categories", { categoryProductList, categoryName, user });
+        })
+        .catch((err) => console.log(err));
     }
   }
 
-  if(req.body.availability){
+  if (req.body.availability) {
     //console.log('heeeere',req.body.availability)
-    Product.find({category: categoryName, availability: req.body.availability})
-    .then(categoryProductList=>{
-      res.render('categories', {categoryProductList, categoryName, user});
+    Product.find({
+      category: categoryName,
+      availability: req.body.availability,
     })
-      .catch(err=>next(err))
-
+      .then((categoryProductList) => {
+        res.render("categories", { categoryProductList, categoryName, user });
+      })
+      .catch((err) => next(err));
   }
+});
 
-})
-
-router.post('/search/results',(req, res, next)=>{
-  console.log(req.body.query)
-  const query=req.body.query;
+router.post("/search/results", (req, res, next) => {
+  const query = req.body.query;
   const user = req.session.passport ? req.session.passport.user : undefined;
 
-  Product.find({name:query})//change the query to search for products which name or description contain the string in the search bar
-    .then(productList=>{
-      //console.log(productList)
-      res.render('all-categories',{productList,user})
+  Product.find() //change the query to search for products which name or description contain the string in the search bar
+    .then((productList) => {
+      const searchedProducts = productList.filter((product) => {
+        return (
+          product.name.toLowerCase().includes(query.toLowerCase()) ||
+          product.description.toLowerCase().includes(query.toLowerCase())
+        );
+      });
+      //console.log(searchedProducts)
+      res.render("all-categories", { user, searchedProducts });
     })
-    .catch(err=>next(err))
-})
+    .catch((err) => next(err));
+});
 
 // router.post('/search/results/filter',(req,res,next)=>{
 
-  // if(req.body.price){
-  //   if (req.body.price.split("-")[1]== ''){
-  //     const lower = req.body.price.split("-")[0];
-  //     Product.find({price: { $gte: lower }})
-  //     .then (categoryProductList => {
-  //       res.render('all-categories', {categoryProductList, user: req.session.passport.user});
-  //     })
-  //     .catch (err => console.log(err))
-      
-  //   }else{
-  //     const lower = req.body.price.split("-")[0];
-  //     const higher = req.body.price.split("-")[1];
-  //     Product.find({$and: [ { price: { $lte: higher } }, { price: { $gt: lower } } ]  })
-  //     .then (categoryProductList => {
-  //       //console.log(categoryProductList,higher,lower)
-  //       res.render('all-categories', {categoryProductList, user: req.session.passport.user});
-  //     })
-  //     .catch (err => console.log(err))
-  //   }
-  // }
+// if(req.body.price){
+//   if (req.body.price.split("-")[1]== ''){
+//     const lower = req.body.price.split("-")[0];
+//     Product.find({price: { $gte: lower }})
+//     .then (categoryProductList => {
+//       res.render('all-categories', {categoryProductList, user: req.session.passport.user});
+//     })
+//     .catch (err => console.log(err))
 
-  // if(req.body.availability){
-  //   console.log('heeeere',req.body.availability)
-  //   Product.find({availability: req.body.availability})
-  //   .then(categoryProductList=>{
-  //     res.render('all-categories', {categoryProductList, user: req.session.passport.user});
-  //   })
-  //     .catch(err=>next(err))
+//   }else{
+//     const lower = req.body.price.split("-")[0];
+//     const higher = req.body.price.split("-")[1];
+//     Product.find({$and: [ { price: { $lte: higher } }, { price: { $gt: lower } } ]  })
+//     .then (categoryProductList => {
+//       //console.log(categoryProductList,higher,lower)
+//       res.render('all-categories', {categoryProductList, user: req.session.passport.user});
+//     })
+//     .catch (err => console.log(err))
+//   }
+// }
 
-  // }
+// if(req.body.availability){
+//   console.log('heeeere',req.body.availability)
+//   Product.find({availability: req.body.availability})
+//   .then(categoryProductList=>{
+//     res.render('all-categories', {categoryProductList, user: req.session.passport.user});
+//   })
+//     .catch(err=>next(err))
 
+// }
 
 // })
 
