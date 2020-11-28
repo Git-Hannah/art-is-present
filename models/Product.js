4fd636db62f173e40e532efde59ae16c2800d0f1
ararita <munjizavesna@gmail.com>
2020-11-28 22:29:07 +0100

    Fix conditional displaying info bug

--- models/Product.js
+++ models/Product.js
@@ -1,45 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const productSchema = new Schema({
- 
   name: {
     type: String,
-    required: [true, 'Name is required!']
+    required: [true, "Name is required!"],
   },
   //ask about:
   price: {
-    type: Number    
+    type: Number,
   },
 
   availability: {
     type: String,
-    enum: ['available', 'currently unavailable'],
-    required: [true, 'Availability info is required!']
+    enum: ["available", "currently unavailable"],
+    required: [true, "Availability info is required!"],
   },
 
   images: [String],
-    
+
   description: {
     type: String,
-    required: [true, 'Description is required!']
+    required: [true, "Description is required!"],
   },
-  owner: [{ 
-    type: Schema.Types.ObjectId, 
-    ref: 'Artist' 
-  }],
+  owner: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Artist",
+    },
+  ],
 
   category: {
     type: String,
-    enum: [
-      'Painting', 
-      'Drawing', 
-      'Photography', 
-      'Performance', 
-      'Music']
-  }
-
+    enum: ["Painting", "Drawing", "Photography", "Performance", "Music"],
+  },
 });
 
-const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
-
